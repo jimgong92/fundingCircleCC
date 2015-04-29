2546fcc0745c4f25430bc247fa8cdf040d623c24
Jimmy Gong <gong.jim@gmail.com>
2015-04-29 16:20:09 -0700

    Adds pseudocode

--- solver.py
+++ solver.py
@@ -1,5 +1,15 @@
+import math
+
 def printPrimeTable(bound):
   table = ''
+  # Get primes within bound
+
+  # Get width of columns (length of biggest number)
+  # Generate initial row of column labels - offset by row labels
+  #   Offset initial row by width of the row labels
+
+  # Generate table rows (row label | product of row * column labels)
+
   print(table)
 
 if (__name__ == '__main__'):
